49d2a2bb973b5f2e652eecbb77f6ecc5ccc058f7
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:06:45 +0200

    Add project files.

---
+++ Tema1_Ex11/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Ex 11
+Folosind instructiunea “switch”, scrieti un program care citind de la tastatura un
+numar intreg va verifica urmatoarele
+“unu” daca numarul citit este 1
+“doi” daca numarul citit este 2
+“trei” daca numarul citit este 3
+“cinci” daca numarul citit este 5
+“opt” daca numarul citit este 8
+“neidentificat” pentru orice alt caz*/
+
+
+Console.WriteLine("Introduceți un număr întreg:");
+int numar = Convert.ToInt32(Console.ReadLine());
+
+switch (numar)
+{
+    case 1:
+        Console.WriteLine("unu");
+        break;
+    case 2:
+        Console.WriteLine("doi");
+        break;
+    case 3:
+        Console.WriteLine("trei");
+        break;
+    case 5:
+        Console.WriteLine("cinci");
+        break;
+    case 8:
+        Console.WriteLine("opt");
+        break;
+    default:
+        Console.WriteLine("neidentificat");
+        break;
+}
+    
